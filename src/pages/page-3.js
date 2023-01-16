e9125dca8e0788742c97cffbea017548c5682a88
Jerboa <lotaijo@gmail.com>
2023-01-16 17:54:41 +1300

    building blog1

---
+++ src/pages/page-3.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+
+const ThirsPage = () => (
+  <Layout>
+    <h1>Hi from the third page</h1>
+  </Layout>
+)
+
+export default ThirsPage
