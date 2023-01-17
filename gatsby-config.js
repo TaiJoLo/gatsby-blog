cb75f011529bf2ab8e533960246b955ca83655b3
Jerboa <lotaijo@gmail.com>
2023-01-17 15:45:42 +1300

    building blog2

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,8 @@ module.exports = {
         path: `${__dirname}/src/markdown-pages`,
       },
     },
-    git`gatsby-transformer-sharp`,
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
